67763760c390e1e309a1e0e6bf0ce632d17c27c9
MathieuDolphin <67150217+MathieuDolphin@users.noreply.github.com>
2025-01-14 20:38:25 +0100

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.tab').forEach(tab => {
-    tab.addEventListener('click', () => {
-        const content = tab.nextElementSibling;
-        const isOpen = content.style.display === 'block';
-        document.querySelectorAll('.tab-content').forEach(tc => tc.style.display = 'none');
-        content.style.display = isOpen ? 'none' : 'block';
-    });
-});
